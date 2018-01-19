77cf7ea1c531dfbd67040491c77cd51ca1d16daa
tianshaojie <tianshaojie@alibaba-inc.com>
2018-01-19 16:07:55 +0800

    double check 单例模式

---
+++ src/com/yuzhi/fine/singleton/DoubleCheckSingleton.java
@@ -0,0 +1,54 @@
+package com.yuzhi.fine.singleton;
+
+/**
+ * @author tianshaojie
+ * @date 2018/1/10
+ */
+public class DoubleCheckSingleton {
+
+    /**
+     * volatile 防止指令重排
+     */
+    private volatile static DoubleCheckSingleton instance;
+
+    /**
+     * 防止反射机制破坏单例
+     */
+    private DoubleCheckSingleton() {
+        if(instance != null) {
+            throw new RuntimeException("error instant");
+        }
+    }
+
+    /**
+     * double check 可以有效降低 synchronized 的性能损耗
+     * @return
+     */
+    public static DoubleCheckSingleton getInstance() {
+        if(instance == null) {
+            synchronized(DoubleCheckSingleton.class) {
+                if(instance == null) {
+                    instance = new DoubleCheckSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    /**
+     * 防止反序列化破坏单例
+     * @return
+     */
+    private Object readResolve(){
+        return instance;
+    }
+
+
+    public void test() {
+        System.out.println("you got me");
+    }
+
+    public static void main(String[] args) {
+        DoubleCheckSingleton.getInstance().test();
+    }
+}
